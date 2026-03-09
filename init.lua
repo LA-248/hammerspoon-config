5ce52ce305f3580ab3049577e9b77c1420986a0c
Luca Antunes <104241771+LA-248@users.noreply.github.com>
2026-03-09 01:16:06 +0100

    update config

--- init.lua
+++ init.lua
@@ -1,39 +1,41 @@
--- Bind Meh + l to lock the screen
-hs.hotkey.bind({ "ctrl", "alt", "shift" }, "l", function()
-    hs.caffeinate.lockScreen()
+local hyper = { "cmd", "ctrl", "alt", "shift" }
+
+-- Bind Hyper + l to lock the screen
+hs.hotkey.bind(hyper, "l", function()
+  hs.caffeinate.lockScreen()
 end)
 
--- Google Chrome keybind (Meh + g)
-hs.hotkey.bind({ "ctrl", "alt", "shift" }, "g", function()
-    hs.application.launchOrFocus("Google Chrome")
+-- Google Chrome keybind (Hyper + f)
+hs.hotkey.bind(hyper, "f", function()
+  hs.application.launchOrFocus("Google Chrome")
 end)
 
--- VS Code keybind (Meh + t)
-hs.hotkey.bind({ "ctrl", "alt", "shift" }, "t", function()
-    hs.application.launchOrFocus("Visual Studio Code")
+-- VS Code keybind (Hyper + r)
+hs.hotkey.bind(hyper, "r", function()
+  hs.application.launchOrFocus("Visual Studio Code")
 end)
 
--- Ghostty keybind (Meh + h)
-hs.hotkey.bind({ "ctrl", "alt", "shift" }, "h", function()
-    hs.application.launchOrFocus("Ghostty")
+-- Ghostty keybind (Hyper + j)
+hs.hotkey.bind(hyper, "j", function()
+  hs.application.launchOrFocus("Ghostty")
 end)
 
--- Spotify keybind (Meh + y)
-hs.hotkey.bind({ "ctrl", "alt", "shift" }, "y", function()
-    hs.application.launchOrFocus("Spotify")
+-- Spotify keybind (Hyper + u)
+hs.hotkey.bind(hyper, "u", function()
+  hs.application.launchOrFocus("Spotify")
 end)
 
--- ChatGPT keybind (Meh + n)
-hs.hotkey.bind({ "ctrl", "alt", "shift" }, "n", function()
-    hs.application.launchOrFocus("ChatGPT")
+-- ChatGPT keybind (Hyper + n)
+hs.hotkey.bind(hyper, "n", function()
+  hs.application.launchOrFocus("ChatGPT")
 end)
 
--- Microsoft Teams keybind (Meh + e)
-hs.hotkey.bind({ "ctrl", "alt", "shift" }, "e", function()
-    hs.application.launchOrFocus("Microsoft Teams")
+-- Microsoft Teams keybind (Hyper + m)
+es.hotkey.bind(hyper, "m", function()
+  hs.application.launchOrFocus("Microsoft Teams")
 end)
 
 -- Reload Hammerspoon config
-hs.hotkey.bind({ "ctrl", "alt", "shift" }, "v", function()
+hs.hotkey.bind(hyper, "v", function()
   hs.reload()
 end)
